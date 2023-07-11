c1b30a2b13e64bb74d80eeec6f307331517c03b7
UwatakiYuichi <fatehotaru@gmail.com>
2023-07-12 04:50:32 +0900

    初期コミット

---
+++ Assets/Script/SampleScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class SampleScript : MonoBehaviour
+{
+
+    private GameObject obj;
+    private int n;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        n = 100;
+
+
+        Debug.Log("戻った");
+        Debug.Log(n);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
